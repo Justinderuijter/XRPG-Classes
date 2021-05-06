2038c29a3c01f6f9716f65ea85fad05f9c7bce39
Justinderuijter <justin.xepos.deruijter@gmail.com>
2021-05-06 22:24:03 +0200

    Removed autodated code

--- src/main/java/me/xepos/rpg/XRPG.java
+++ src/main/java/me/xepos/rpg/XRPG.java
@@ -1,7 +1,6 @@
 package me.xepos.rpg;
 
 import me.xepos.rpg.commands.ChangeClassCommand;
-import me.xepos.rpg.commands.SmokeBombCommand;
 import me.xepos.rpg.commands.XRPGDebug;
 import me.xepos.rpg.commands.XRPGReload;
 import me.xepos.rpg.configuration.ClassLoader;
@@ -83,7 +82,6 @@ public final class XRPG extends JavaPlugin {
         //registering listeners/commands
         initEventListeners();
 
-        this.getCommand("smoke").setExecutor(new SmokeBombCommand());
         this.getCommand("xrpgdebug").setExecutor(new XRPGDebug(this, classData));
         this.getCommand("xrpgreload").setExecutor(new XRPGReload());
         this.getCommand("changeclass").setExecutor(new ChangeClassCommand(this, inventoryGUI));
@@ -111,76 +109,7 @@ public final class XRPG extends JavaPlugin {
                 inventoryGUI.setItem(slot, item);
             }
         }
-
-        /*inventoryGUI.setItem(17, buildItemStack(Material.BLAZE_ROD, "Wizard", new ArrayList<String>(){{
-            add("A long range class with limited melee potential");
-            add("Cast fire, wind and ice spells");
-            add("Empower spells by maintaining fireball stacks");
-            add("High damage, medium crowd control and low mobility");
-        }}));
-
-        inventoryGUI.setItem(16, buildItemStack(Material.DIAMOND_AXE, "Ravager", new ArrayList<String>(){{
-            add("A melee class focused on dealing AoE damage");
-            add("Attacking with an axe grants rage");
-            add("Bonus effects scaling with rage level");
-            add("High damage, medium mobility and low crowd control");
-        }}));
-
-        inventoryGUI.setItem(15, buildItemStack(Material.BOW, "Ranger", new ArrayList<String>(){{
-            add("A ranged class focused using the bow");
-            add("Switch between different arrow types");
-            add("Add tipped arrows to the mix for even more power");
-            add("High damage, medium mobility and low crowd control");
-        }}));
-
-        inventoryGUI.setItem(14, buildItemStack(Material.NETHERITE_HOE, "Necromancer", new ArrayList<String>(){{
-            add("A melee class focused on applying status effects");
-            add("Fallen opponents will join your army of the undead");
-            add("Each with their own unique attributes");
-            add("medium damage, medium mobilty, high crowd control");
-        }}));
-
-        inventoryGUI.setItem(13, buildItemStack(Material.SHIELD, "Guardian", new ArrayList<String>(){{
-            add("A tanky melee class focused on survivability");
-            add("Call upon Aegis' to protect you and your allies");
-            add("Draw the attention of nearby mobs with your attacks");
-            add("medium damage, low mobilty, high defense");
-        }}));
-
-        inventoryGUI.setItem(12, buildItemStack(Material.POPPY, "Brawler", new ArrayList<String>(){{
-            add("A melee class that uses its fists as its weapon");
-            add("Draw power from your armor");
-            add("Choice between healing and extra damage");
-            add("high damage, low mobilty, no crowd control");
-        }}));
-
-        inventoryGUI.setItem(11, buildItemStack(Material.ENCHANTED_GOLDEN_APPLE, "Bard", new ArrayList<String>(){{
-            add("A support class focused on keeping your allies alive");
-            add("Consume golden apples or potions to heal your allies");
-            add("Grant yourself and an ally invulnerability");
-            add("medium damage, low mobilty, high crowd control");
-        }}));
-
-        inventoryGUI.setItem(10, buildItemStack(Material.DIAMOND_SWORD, "Assassin", new ArrayList<String>(){{
-            add("A melee class focused on striking its enemies from behind");
-            add("Execute low health targets");
-            add("Grant yourself stealth and get backstrikes");
-            add("medium damage, high mobilty, low crowd control");
-        }}));*/
     }
-
-/*    public ItemStack buildItemStack(Material material, String displayName, List<String> lore)
-    {
-        ItemStack item = new ItemStack(material);
-        ItemMeta meta = item.getItemMeta();
-        if (meta != null){
-            meta.setDisplayName(displayName);
-            meta.setLore(lore);
-            meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
-            item.setItemMeta(meta);
-        }
-        return item;
-    }*/
 
     private void initEventListeners() {
         getServer().getPluginManager().registerEvents(new ClassListener(this, databaseManager), this);
